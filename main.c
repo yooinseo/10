34f931cb19786795f5e57eed2c423a2719d3f9d8
yooinseo <qetu37@sookmyung.ac.kr>
2025-11-06 15:36:55 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-   char src[] = "The Worst things to eat before you sleep";
-   char dst[100];
+   char str[30] = "Happy C programming";
    
-   strcpy(dst,src);
-   
-   printf("copied string : %s\n", dst);
+   printf("string %s length : %i\n", str, strlen(str));
   
   system("PAUSE");	
   return 0;
